232a5f4a1e766b4b6c3e0f9f925c9c2db92c4372
Felps03 <felipe.031293@gmail.com>
2019-04-08 16:29:01 -0300

    excluindo arquivos

--- js/boot.js
+++
@@ -1,6 +0,0 @@
-import {currentInstance} from './controllers/TimeController';
-import {} from './polyfill/fetch';
-
-let timeController = currentInstance();
-
-document.querySelector('.form').onsubmit = TimeController.adiciona.bind(timeController);
