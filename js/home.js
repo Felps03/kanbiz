152b0017cf0d197ab5be26d8f45420e94d439d21
Felps03 <felipe.031293@gmail.com>
2019-04-22 20:48:18 -0300

    refatoração de codigo

--- js/home.js
+++ js/home.js
@@ -1,17 +1,19 @@
 import 'bootstrap';
 import $ from 'jquery';
 
+import { ProjetoController } from './controllers/ProjetoController';
 import { TimeController } from './controllers/TimeController.js';
 import { ColaboradorController } from './controllers/ColaboradorController';
 
-
+const projetoController = new ProjetoController();
 const timeController = new TimeController();
 const colaboradorController = new ColaboradorController();
 
-
 const form = document.querySelector('form');
-
 form.addEventListener('submit', timeController.adicionaTime.bind(timeController));
+
+const formProjeto = document.querySelector('.formProjeto');
+formProjeto.addEventListener('submit', projetoController.adicionaProjeto.bind(projetoController));
 
 $(document).on("click", ".aceita", function () {
   // let chaveTime = $("table tr:nth-child(2)").attr('id');
@@ -22,24 +24,19 @@ $(document).on("click", ".deleta", function () {
   timeController.recusarColaboradorTime();
 });
 
-//TODO: Rever
-$('#orderModal').modal({
-  keyboarnd: true,
-  backdrop: "static",
-  show: false,
-}).on('show.bs.modal', function () {
-  var getIdFromRow = $(this).data('orderid');
-  var id = $(this).data('id');
-  alert(id);
-  $(this).find('#orderDetails').html($('<b> O ID eh: ' + getIdFromRow + '</b>'))
-});
 
-$(".table-striped").find('tr[data-target]').on('click', function () {
-  $('#orderModal').data('orderid', $(this).data('id'));
-});
+// //TODO: Rever
+// $('#orderModal').modal({
+//   keyboarnd: true,
+//   backdrop: "static",
+//   show: false,
+// }).on('show.bs.modal', function () {
+//   var getIdFromRow = $(this).data('orderid');
+//   var id = $(this).data('id');
+//   alert(id);
+//   $(this).find('#orderDetails').html($('<b> O ID eh: ' + getIdFromRow + '</b>'))
+// });
 
-
-
-/**
- *
- */
+// $(".table-striped").find('tr[data-target]').on('click', function () {
+//   $('#orderModal').data('orderid', $(this).data('id'));
+// });
